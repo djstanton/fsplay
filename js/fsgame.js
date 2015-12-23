868151cb91ee9c773f0756f298e4ba54c8fc3e89
Anton <djst.anton@mail.ru>
2015-12-23 20:32:02 +0300

    q2331

--- js/fsgame.js
+++ js/fsgame.js
@@ -5,16 +5,17 @@ fsgame = {
     coinStorage: 0,
     teamId: 1,
     keycodeConfig: {
-        49 : 1,
-        50 : 2,
-        51 : 3,
-        52 : 4,
-        53 : 5,
-        54 : 6,
-        55 : 7,
-        56 : 8
+        1: 48,
+        2 : 55,
+        3 : 53,
+        4 : 110,
+        5 : 50,
+        6 : 109,
+        7 : 56,
+        8 : 54
     },
-    gameInterval: 5000,
+    step: 0,
+    gameInterval: 20000,
     coinInterval: 500,
     playI: 0,
     x2: false,
@@ -51,9 +52,82 @@ fsgame = {
         _this.x2 = 0;
         _this.startGame();
     },
+    setActiveStep:function(step){
+        $('.fs-game').attr('data-state',step);
+        this.step = step;
+    },
+    startTimer: function(){
+        var _this = this, lastTime = _this.gameInterval/1000;
+        _this.timerInterval = setInterval(function(){
+            lastTime--;
+            $('.timer-text').html(lastTime);
+            if(!lastTime){
+                clearInterval(_this.timerInterval);
+                _this.finish();
+            }
+        }, 1000);
+    },
+    nextStep:function(e){
+        var _this = this, step = this.step,
+            rightHand = $('.man-hand-even'), leftHand = $('.man-hand-odd');
+        if(step && step == 1){
+            _this.setActiveStep(2);
+            setTimeout(function(){
+                _this.play(1);
+            }, 1000);
+        } else if(step && step == 2){
+            switch (e.keyCode) {
+                case _this.keycodeConfig[1]:
+                    _this.currentHand = 1;
+                    leftHand.attr('data-pos', 7);
+                    rightHand.attr('data-pos', -1);
+                    debugger;
+                    break;
+                case _this.keycodeConfig[2]:
+                    _this.currentHand = 2;
+                    rightHand.attr('data-pos', 8);
+                    leftHand.attr('data-pos', -1);
+                    break;
+                case _this.keycodeConfig[3]:
+                    _this.currentHand = 3;
+                    leftHand.attr('data-pos', 5);
+                    rightHand.attr('data-pos', -1);
+                    break;
+                case _this.keycodeConfig[4]:
+                    _this.currentHand = 4;
+                    rightHand.attr('data-pos', 6);
+                    leftHand.attr('data-pos', -1);
+                    break;
+                case _this.keycodeConfig[5]:
+                    _this.currentHand = 5;
+                    leftHand.attr('data-pos', 3);
+                    rightHand.attr('data-pos', -1);
+                    break;
+                case _this.keycodeConfig[6]:
+                    _this.currentHand = 6;
+                    rightHand.attr('data-pos', 4);
+                    leftHand.attr('data-pos', -1);
+                    break;
+                case _this.keycodeConfig[7]:
+                    _this.currentHand = 7;
+                    leftHand.attr('data-pos', 1);
+                    rightHand.attr('data-pos', -1);
+                    break;
+                case _this.keycodeConfig[8]:
+                    _this.currentHand = 8;
+                    rightHand.attr('data-pos', 2);
+                    leftHand.attr('data-pos', -1);
+                    break;
+            }
+            console.log(_this.currentHand);
+        } else {
+            _this.setActiveStep(1);
+        }
+    },
     init: function(){
         var _this = this;
         clearInterval(_this.playI);
+        _this.startTimer();
         _this.playI = setInterval(function(){
             _this.coinGenerator.makeCoin();
         }, _this.getRandomInt((_this.x2 ? 75 : 300), (_this.x2 ? 200 : 800)));
@@ -61,7 +135,6 @@ fsgame = {
         if(!_this.isStarted){
             setTimeout(function(){
                 clearInterval(_this.playI);
-                console.log('Вы собрали хуй');
                 console.log('Выпало '+fsgame.coinGenerator.coinsCounter+' монет');
             }, _this.gameInterval);
         }
@@ -71,10 +144,21 @@ fsgame = {
     play: function(teamId){
        var _this = this;
         _this.teamId = teamId;
-       _this.init();
+        setTimeout(function(){
+            _this.init();
+        }, 3000);
+
     },
     finish: function(){
+        var _this = this;
+        $('body').append('<div class="result">Вы собрали '+_this.coinGenerator.collectedCoins+' ФМ</div>');
 
+        _this.coinGenerator.collectedCoins = 0;
+        _this.gameInterval = 120000;
+        setTimeout(function(){
+            _this.setActiveStep(0);
+            $('.result').fadeOut();
+        }, 5000);
     },
     setFallTime: function(){
         var _this = this;
@@ -89,9 +173,8 @@ fsgame = {
             var data = {
                 slotId: fsgame.getRandomInt(0,7),
                 isBomb: fsgame.getRandomInt(0, 7),
-                speed: fsgame.getRandomInt(1, 3)
+                speed: 1600
             };
-            data.speed = (data.speed/2)*1000;
             if(data.isBomb !== 1){
                 fsgame.coinGenerator.coinsCounter++;
             }
@@ -112,7 +195,10 @@ fsgame = {
         setTimeout(function(){
             if(data.slotId == _this.currentHand) {
                 if(data.isBomb == 1){
-                    fsgame.coinGenerator.collectedCoins = fsgame.coinGenerator.collectedCoins-5;
+                    fsgame.coinGenerator.collectedCoins = fsgame.coinGenerator.collectedCoins-3;
+                    if(fsgame.coinGenerator.collectedCoins < 0){
+                        fsgame.coinGenerator.collectedCoins = 0;
+                    }
                     $(_this.scoreWrap).html(fsgame.coinGenerator.collectedCoins);
                 } else {
                     fsgame.coinGenerator.collectedCoins++;
